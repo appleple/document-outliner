08303e6e56888de48d51299aca788afd3de3a6da
atsu666 <ito.atsu666@gmail.com>
2018-12-19 19:30:07 +0900

    fix

--- lib/type.d.ts
+++ lib/type.d.ts
@@ -8,6 +8,7 @@ declare type OptionsType = {
     listType?: 'ul' | 'ol';
     listClassName?: string;
     itemClassName?: string;
+    linkClassName?: string;
     anchorName?: string;
     levelLimit?: number;
 };
